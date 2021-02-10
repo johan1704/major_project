0ea3adbf3666d250577f7ef09b23b0be85a06aef
johan1704 <68570240+johan1704@users.noreply.github.com>
2021-02-10 17:53:02 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,42 @@
+from flask import Flask,request, url_for, redirect, render_template
+import pickle
+import pandas as pd
+
+app = Flask(__name__)
+
+model = pickle.load(open("xgboost_2.pkl", "rb"))
+
+
+@app.route('/')
+def hello_world():
+    return render_template("index.html")
+
+
+@app.route('/predict',methods=['POST','GET'])
+def predict():
+    text1 = request.form['1']
+    text2 = request.form['2']
+    text3 = request.form['3']
+    text4 = request.form['4']
+    text5 = request.form['5']
+    text6 = request.form['6']
+    text7 = request.form['7']
+    text8 = request.form['8']
+    text9 = request.form['9']
+    text10 = request.form['10']
+ 
+    row_df = pd.DataFrame([pd.Series([text1,text2,text3,text4,text5,text6,text7,text8,text9,text10])])
+    print(row_df)
+    pred=model.predict(row_df)
+    prediction=model.predict_proba(row_df)
+    output='{0:.{1}f}'.format(prediction[0][1], 2)
+    output = str(float(output)*100)
+    if output>str(0.5):
+        return render_template('index.html',pred=f'.\nPercentage to leave of the customer is {output} %')
+    else:
+        return render_template('index.html',pred=f'.\n Percentage to leave of the customer is {output} %')
+    
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
